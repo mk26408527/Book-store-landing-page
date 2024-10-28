4d415cbe3b8338bbce9772f1f1f4c381f6d135b8
M.Huzaifa200 <mk26408527@gmail.com>
2024-10-28 05:16:08 +0500

    Make Design  Perfect

---
+++ script.js
@@ -0,0 +1,62 @@
+const books = [
+    {
+      title: "Book Title 1",
+      author: "Author 1",
+      price: "$19.99",
+      image: "Image 1",
+      section: "best-sellers"
+    },
+    {
+      title: "Book Title 2",
+      author: "Author 2",
+      price: "$24.99",
+      image: "Image 2",
+      section: "best-sellers"
+    },
+    {
+      title: "Book Title 3",
+      author: "Author 3",
+      price: "$29.99",
+      image: "Image 3",
+      section: "best-sellers"
+    },
+    {
+      title: "Book Title 4",
+      author: "Author 4",
+      price: "$17.99",
+      image: "Image 4",
+      section: "new-arrivals"
+    },
+    {
+      title: "Book Title 5",
+      author: "Author 5",
+      price: "$22.99",
+      image: "Image 5",
+      section: "new-arrivals"
+    },
+    {
+      title: "Book Title 6",
+      author: "Author 6",
+      price: "$18.99",
+      image: "Image 6",
+      section: "new-arrivals"
+    },
+    {
+      title: "Book Title 7",
+      author: "Author 7",
+      price: "$20.99",
+      image: "Image 7",
+      section: "readers-choice"
+    },
+    {
+      title: "Book Title 8",
+      author: "Author 8",
+      price: "$23.99",
+      image: "Image 8",
+      section: "readers-choice"
+    },
+    {
+      title: "Book Title 9",
+      author: "Author 9",
+      price: "$25.99"
+    }]  
